255948b9973ba7be49fe57623ba7c10836c62c36
Iskald <>
2025-08-13 11:07:06 +0200

    fixed config key placement

--- Interface/Data/SpriteProviderConfig.cs
+++ Interface/Data/SpriteProviderConfig.cs
@@ -7,6 +7,8 @@ namespace IceCold.UI.Interface
     [CreateAssetMenu(fileName = nameof(SpriteProviderConfig), menuName = "IceCold/UI/Create Sprite Provider", order = 0)]
     public class SpriteProviderConfig : IceColdConfig
     {
+        public override string Key => nameof(SpriteProviderConfig);
+        
         public Sprite falloutSprite = null;
         public List<IconData> sprites = new();
         public List<ColorData> colors = new();
--- Interface/Data/UIConfig.cs
+++ Interface/Data/UIConfig.cs
@@ -6,6 +6,8 @@ namespace IceCold.UI.Interface
     [CreateAssetMenu(fileName = nameof(UIConfig), menuName = "IceCold/UI/Create Config", order = 0)]
     public class UIConfig : IceColdConfig
     {
+        public override string Key => nameof(UIConfig);
+        
         public int canvasSortingOrder = 10;
         public RenderMode canvasRenderMode = RenderMode.ScreenSpaceOverlay;
     }
